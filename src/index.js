395520c1ee23bf0765ba39d4752c2bd810bf163c
ankhudyakov <Duhless153>
2022-11-08 15:03:33 +0300

    adding build

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('page-wrapper'));
-root.render(<App />);
